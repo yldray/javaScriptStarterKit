3e7dc71e137b0417c281f6ca93cbd869770f6787
Yildiray Tasbas <tasbasyildiray@outlook.com>
2021-10-24 22:36:03 +0300

    js ders 1 sonu

---
+++ src/app.js
@@ -0,0 +1,38 @@
+console.log("Merhaba Kodlama IO")
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+dolarDun ="9.20"
+
+{
+    let dolarDun = 9.10
+} 
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 // bunu yapamıyoruz yasak const oldugu ıcın
+
+console.log(euroDun) 
+
+//array
+//camelCasing
+//PascalCasing 
+let konutKredileri  = ["Konut kredisi ","Emlak Konut Kredisi"," Kamu Konut Kredisi","Özel Kredi"]
+
+//let konutKredileri  = [""Konut kredisi ","Emlak Konut Kredisi"," Kamu Konut Kredisi"]
+//let users = getUsersFromApi()
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+        <li> Konut kredisi</li>
+        <li>Emlak Kredisi</li>
+        <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
